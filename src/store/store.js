d573d7ef71f547fab68a7c27ee8ec6d759347347
yevhenMedovnyk <yevhenmed@gmail.com>
2023-06-30 19:38:43 +0300

    some fixes in redux store file

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,8 @@ import plants from "./Slices/mainSlice";
 import cart from "./Slices/cartSlice";
 import sort from "./Slices/sortSlice";
 import auth from "./Slices/authSlice";
+import wishlist from "./Slices/wishlistSlice";
+import orders from "./Slices/ordersSlice";
 
 export const store = configureStore({
 	reducer: {
