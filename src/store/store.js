a1d6bda845cee414f6a37b7d8b7de7683dde47b2
yevhenMedovnyk <yevhenmed@gmail.com>
2023-06-30 19:28:27 +0300

    some little fixes

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,11 @@ import plants from "./Slices/mainSlice";
 import cart from "./Slices/cartSlice";
 import sort from "./Slices/sortSlice";
 import auth from "./Slices/authSlice";
+<<<<<<< Updated upstream
+=======
+import wishlist from "./Slices/wishlistSlice";
+import orders from "./Slices/ordersSlice";
+>>>>>>> Stashed changes
 
 export const store = configureStore({
 	reducer: {
